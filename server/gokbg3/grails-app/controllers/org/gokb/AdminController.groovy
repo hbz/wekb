ea5b761e93d6b9a2d4b24d881834c1f1b320937e
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2022-03-22 15:29:25 +0100

    New Function: findTippDuplicates

--- server/gokbg3/grails-app/controllers/org/gokb/AdminController.groovy
+++ server/gokbg3/grails-app/controllers/org/gokb/AdminController.groovy
@@ -697,4 +697,21 @@ class AdminController {
     def result = [:]
     result
   }
+
+  def findTippDuplicates() {
+    log.debug("findTippDuplicates::${params}")
+    def result = [:]
+
+    params.pkg = Package.get(40893351)
+
+    List<TitleInstancePackagePlatform> tippsWithUrlDuplicates = TitleInstancePackagePlatform.executeQuery("select tipp from TitleInstancePackagePlatform as tipp, Combo as pkg_combo" +
+            " where pkg_combo.toComponent=tipp and pkg_combo.fromComponent = :pkg and " +
+            " tipp.url in (select tipp2.url from TitleInstancePackagePlatform tipp2, Combo as pkg_combo2 where pkg_combo2.toComponent=tipp2 and pkg_combo2.fromComponent = :pkg group by tipp2.url having count(tipp2.url) > 1)" +
+            " order by tipp.url",
+            [pkg: params.pkg])
+
+    result.tipps = tippsWithUrlDuplicates
+
+    result
+  }
 }
