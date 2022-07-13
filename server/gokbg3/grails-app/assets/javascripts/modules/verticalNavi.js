28bcb1a43d11f631448da96742361add721f501b
bluoss <ingrid.bluoss@hbz-nrw.de>
2022-07-13 15:02:50 +0200

    ERMS-4349 no console.log

--- server/gokbg3/grails-app/assets/javascripts/modules/verticalNavi.js
+++ server/gokbg3/grails-app/assets/javascripts/modules/verticalNavi.js
@@ -8,24 +8,18 @@ verticalNavi = {
     },
 
     init: function () {
-        console.log('verticalNavi.init')
-
-
         $("#toc").sidebar("setting", "dimPage", false);
         if (matchMedia) {
-            console.log("matchMedia")
             var mq = window.matchMedia("(max-width: 992px)");
             mq.addEventListener("change",big_or_small);
             big_or_small(mq);
         }
 
         function big_or_small(mq) {
-            console.log("big_or_small")
             // The sidebar *pushes* the pusher, the main content, so we
             // add a class that reduces the pusher's width so the edge
             // content isn't cut off.
             if (mq.matches) {
-                console.log("mq.matches")
                 $("#toc").sidebar("hide");
                 $("#main").removeClass("shrink")
             } else {
