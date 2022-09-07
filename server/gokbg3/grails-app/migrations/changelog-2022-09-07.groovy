0c00840b02e10628b15ac24c7a293d93482d855f
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2022-09-07 21:51:11 +0200

    ERMS-4433: Platform: Open Athens Feld

---
+++ server/gokbg3/grails-app/migrations/changelog-2022-09-07.groovy
@@ -0,0 +1,12 @@
+databaseChangeLog = {
+
+    changeSet(author: "djebeniani (generated)", id: "1662579394414-1") {
+        addColumn(tableName: "platform") {
+            column(name: "plat_open_athens_fk_rv", type: "int8")
+        }
+    }
+
+    changeSet(author: "djebeniani (generated)", id: "1662579394414-2") {
+        addForeignKeyConstraint(baseColumnNames: "plat_open_athens_fk_rv", baseTableName: "platform", constraintName: "FKhyjognfgw6ne6nqvis5d4uw4y", deferrable: "false", initiallyDeferred: "false", referencedColumnNames: "rdv_id", referencedTableName: "refdata_value")
+    }
+}
