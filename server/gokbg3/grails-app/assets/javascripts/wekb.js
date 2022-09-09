45f469f8708b256a2c4d71f437c42e69c17808ae
Moetez16 <moetez.djebeniani@hbz-nrw.de>
2022-09-09 11:15:39 +0200

    ERMS-4462: rote Fehlermeldung: pop Up Kasten

--- server/gokbg3/grails-app/assets/javascripts/wekb.js
+++ server/gokbg3/grails-app/assets/javascripts/wekb.js
@@ -268,5 +268,14 @@ $(function () {
         });
     });
 
+    $('.message .close')
+        .on('click', function() {
+            $(this)
+                .closest('.message')
+                .transition('fade')
+            ;
+        })
+    ;
+
 });
 
